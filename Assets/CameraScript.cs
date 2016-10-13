7eeb3c96e85ce7138ae7106a89502f1386476d86
aam <aam@anahoret.com>
2016-10-13 16:31:25 +0300

    Initial commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+
+		transform.Rotate(-z, 0, 0);
+		transform.Rotate(0, x, 0);
+	}
+}
---
+++ Assets/PlayerStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerStart : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		MovieTexture movieTexture = ((MovieTexture)GetComponent<Renderer> ().material.mainTexture);
+		movieTexture.loop = true;
+		movieTexture.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
+
